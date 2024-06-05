e7cb556a37e69527dd450f56e0a2222b974a0cae
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-06-05 22:24:02 +0200

    PWA Example

---
+++ src/app/components/image/image.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ImagesService } from '../../services/images.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Image } from '../../models/image.interface';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.css'
+})
+export class ImageComponent implements OnInit {
+
+  image: Image;
+
+  constructor(private imagesService: ImagesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    const identifier = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('Identifier --> ', identifier);
+
+    this.imagesService.getImageById(identifier).subscribe((image) => {
+
+      if(!image){
+        return this.router.navigateByUrl('/');
+      }
+
+      this.image = image;
+      console.log('Image --> ', this.image);
+    });
+  }
+}
